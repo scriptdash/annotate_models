77f3d29ad238a7c593d428762b6e279f06e5c649
Dan McAulay <dan.mcaulay@alto.com>
2021-08-02 11:43:48 -0700

    fix rubocop errors

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -2729,7 +2729,7 @@ describe AnnotateModels do
         EOS
         annotate_one_file position: :before
         expect(File.read(@model_file_name)).to eq(
-          <<~HEREDOC,
+          <<~HEREDOC
             module Foo
               # == Schema Info
               #
@@ -2786,7 +2786,7 @@ describe AnnotateModels do
         EOS
         annotate_one_file position: :after
         expect(File.read(@model_file_name)).to eq(
-          <<~HEREDOC,
+          <<~HEREDOC
             module Foo
               # my docs
               class User < ActiveRecord::Base
